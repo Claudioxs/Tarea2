526a6eaae7b4a8ca752fbe2322759b090a8ce85b
franciscamanquecheo <f.manquecheo01@ufromail.cl>
2023-05-23 12:12:45 -0400

    se eliminan import no usados

--- demo/.history/src/main/java/demoDesafio/demo/DemoApplication_20230523120647.java
+++ demo/.history/src/main/java/demoDesafio/demo/DemoApplication_20230523120647.java
@@ -1,6 +1,6 @@
 package demoDesafio.demo;
 
-
+import demoDesafio.demo.service.AutomovilServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
